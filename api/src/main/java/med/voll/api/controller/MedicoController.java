eac1cebf3d1928e0c0d87e09607d03a79cf2f349
Abner Vital <avgchaves@gmail.com>
2025-06-27 15:48:39 -0300

    feat: injeta MedicoRepository e cadastra médico no banco

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,6 +1,10 @@
 package med.voll.api.controller;
 
+import med.voll.api.endereco.Endereco;
 import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.Medico;
+import med.voll.api.repository.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,10 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("medicos")
 public class MedicoController {
+    @Autowired
+    private MedicoRepository medicoRepository;
 
     @PostMapping()
-    public void Cadastrar(@RequestBody DadosCadastroMedico dados){
-        System.out.println(dados);
+    public void cadastrar (@RequestBody DadosCadastroMedico dados){
+        medicoRepository.save(new Medico(dados));
     }
 
 }

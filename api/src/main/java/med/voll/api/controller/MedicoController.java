19bb713ab859d1a38aab17a268c971cfc1955223
Abner Vital <avgchaves@gmail.com>
2025-06-27 17:53:06 -0300

    feat: adiciona anotação @Transactional ao método de cadastro

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,5 +1,6 @@
 package med.voll.api.controller;
 
+import jakarta.transaction.Transactional;
 import med.voll.api.endereco.Endereco;
 import med.voll.api.medico.DadosCadastroMedico;
 import med.voll.api.medico.Medico;
@@ -17,6 +18,7 @@ public class MedicoController {
     private MedicoRepository medicoRepository;
 
     @PostMapping()
+    @Transactional
     public void cadastrar (@RequestBody DadosCadastroMedico dados){
         medicoRepository.save(new Medico(dados));
     }

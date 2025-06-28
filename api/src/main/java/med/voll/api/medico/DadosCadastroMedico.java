0085dbb639b4f7dd797b7c3d77f8b0d5a383273a
Abner Vital <avgchaves@gmail.com>
2025-06-28 11:46:43 -0300

    feat: adiciona validações com Jakarta Bean Validation ao DTO de cadastro de médico

--- api/src/main/java/med/voll/api/medico/DadosCadastroMedico.java
+++ api/src/main/java/med/voll/api/medico/DadosCadastroMedico.java
@@ -1,12 +1,28 @@
 package med.voll.api.medico;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import med.voll.api.endereco.DadosEndereco;
 
 public record DadosCadastroMedico(
+        @NotBlank //valida se o campo está nulo ou ou vázio
         String nome,
+
+        @NotBlank
+        @Email
         String email,
+
+        @NotBlank
+        @Pattern(regexp = "\\d{4,6}") //valida se o campo tem 4-6 digitos
         String crm,
+
+        @NotNull
         Especialidade especialidade,
-        DadosEndereco endereco
+
+        //é necessário colocar o @valid para o Spring poder validar o outro DTO.
+        @NotNull @Valid DadosEndereco endereco
 ) {
 }

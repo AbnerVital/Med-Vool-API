d9ab0abb4b992e44a9024766def9728a2611945b
Abner Vital <avgchaves@gmail.com>
2025-07-02 07:14:14 -0300

    refactor: remove DTO MedicoDTO não utilizado

--- api/src/main/java/med/voll/api/medico/MedicoDTO.java
+++
@@ -1,7 +0,0 @@
-package med.voll.api.medico;
-
-public record MedicoDTO(String nome,
-                        String email,
-                        String crm,
-                        Especialidade especialidade) {
-}

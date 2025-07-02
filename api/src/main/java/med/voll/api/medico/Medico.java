b058ddf93f69f88860a8e6daf4ae22527794585e
Abner Vital <avgchaves@gmail.com>
2025-07-02 14:49:24 -0300

    feat: adiciona campo ativo e método de inativação lógica no modelo Medico

--- api/src/main/java/med/voll/api/medico/Medico.java
+++ api/src/main/java/med/voll/api/medico/Medico.java
@@ -27,10 +27,13 @@ public class Medico {
     @Enumerated(EnumType.STRING)
     private Especialidade especialidade;
 
+    private Boolean ativo;
+
     @Embedded
     private Endereco endereco;
 
     public Medico(DadosCadastroMedico dados) {
+        this.ativo = true;
         this.nome = dados.nome();
         this.email = dados.email();
         this.telefone = dados.telefone();
@@ -49,6 +52,10 @@ public class Medico {
         if(this.endereco != null){
             this.endereco.atualizarInformacoes(dados.endereco());
         }
+    }
+
+    public void inativarMedico() {
+        this.ativo = false;
     }
 }
 

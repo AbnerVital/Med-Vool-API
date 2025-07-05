7535b3e81cfdcadd46c00f28277705b9e065a382
Abner Vital <avgchaves@gmail.com>
2025-07-05 09:16:17 -0300

    refactor(controller): melhora respostas HTTP com ResponseEntity nos endpoints de médico

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -2,19 +2,14 @@ package med.voll.api.controller;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
-import med.voll.api.medico.DadosAtualizacaoMedico;
-import med.voll.api.medico.DadosCadastroMedico;
-import med.voll.api.medico.DadosListagemMedico;
-import med.voll.api.medico.Medico;
-import med.voll.api.repository.MedicoRepository;
+import med.voll.api.medico.*;
 import med.voll.api.service.MedicoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("medicos")
@@ -32,18 +27,22 @@ public class MedicoController {
     }
 
     @GetMapping()
-    public Page<DadosListagemMedico> listar (@PageableDefault(size = 2, sort = {"nome"}) Pageable paginacao){
-        return medicoService.listagemDeMedico(paginacao);
+    public ResponseEntity <Page<DadosListagemMedico>> listar (@PageableDefault(size = 2, sort = {"nome"}) Pageable paginacao){
+        var page = medicoService.listagemDeMedico(paginacao);
+        return ResponseEntity.ok(page);
     }
 
     @PutMapping()
-    public void atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados){
-        medicoService.atualizaDadosMedico(dados);
+    public ResponseEntity atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados){
+        var medico = medicoService.atualizaDadosMedico(dados);
+        return ResponseEntity.ok(new DadosDetalhamentoMedico(medico));
     }
 
     @DeleteMapping("/{id}")
-    public void excluir (@PathVariable Long id){
+    public ResponseEntity excluir (@PathVariable Long id){
         medicoService.exluirDadosMedico(id);
+
+        return ResponseEntity.noContent().build();
     }
 
 

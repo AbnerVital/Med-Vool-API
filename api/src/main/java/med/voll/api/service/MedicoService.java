3ff9d110f438a2f7a6779042d5ecb97b1d198e14
Abner Vital <avgchaves@gmail.com>
2025-07-05 11:45:03 -0300

    feat(service): implementa lógica de detalhamento de médico por ID

--- api/src/main/java/med/voll/api/service/MedicoService.java
+++ api/src/main/java/med/voll/api/service/MedicoService.java
@@ -1,10 +1,7 @@
 package med.voll.api.service;
 
 import jakarta.transaction.Transactional;
-import med.voll.api.medico.DadosAtualizacaoMedico;
-import med.voll.api.medico.DadosListagemMedico;
-import med.voll.api.medico.Medico;
-import med.voll.api.medico.MedicoRepository;
+import med.voll.api.medico.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -36,5 +33,11 @@ public class MedicoService {
 //        executa uma exclusão lógica(não exlcui do banco de dados)
         var medico = medicoRepository.getReferenceById(id);
         medico.inativarMedico();
+    }
+
+    public DadosDetalhamentoMedico detalhamentoMedico(Long id) {
+        var medico = medicoRepository.getReferenceById(id);
+        var dtoMedico= new DadosDetalhamentoMedico(medico);
+        return dtoMedico;
     }
 }

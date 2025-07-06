1c066e6b851758d65cfdd4cf246e80f8dcc9896c
Abner Vital <avgchaves@gmail.com>
2025-07-06 15:05:37 -0300

    refactor(medico): atualiza regras de serviço e retorna DTO de detalhamento

--- api/src/main/java/med/voll/api/service/MedicoService.java
+++ api/src/main/java/med/voll/api/service/MedicoService.java
@@ -1,7 +1,7 @@
 package med.voll.api.service;
 
 import jakarta.transaction.Transactional;
-import med.voll.api.medico.*;
+import med.voll.api.domain.medico.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;

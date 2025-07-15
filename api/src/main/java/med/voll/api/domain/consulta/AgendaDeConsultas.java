de092ebf379d88a3f2d3d403d0d7ad7f7be16a1c
Abner Vital <avgchaves@gmail.com>
2025-07-15 15:17:02 -0300

    feat(consulta): adiciona cancelamento de consultas com motivo

---
+++ api/src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
@@ -0,0 +1,21 @@
+package med.voll.api.domain.consulta;
+
+import med.voll.api.domain.ValidacaoException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AgendaDeConsultas {
+
+    @Autowired
+    private ConsultaRepository consultaRepository;
+
+    public void cancelar(DadosCancelamentoConsulta dados) {
+        if (!consultaRepository.existsById(dados.idConsulta())) {
+            throw new ValidacaoException("Id da consulta informado não existe!");
+        }
+
+        var consulta = consultaRepository.getReferenceById(dados.idConsulta());
+        consulta.cancelar(dados.motivo());
+    }
+}

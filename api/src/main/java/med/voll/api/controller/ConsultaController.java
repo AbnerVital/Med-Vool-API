7f2119bba735fb3fbc8f8b982034de7435c7f137
Abner Vital <avgchaves@gmail.com>
2025-07-15 15:13:47 -0300

    feat(consulta): adiciona agendamento de consultas com validações

---
+++ api/src/main/java/med/voll/api/controller/ConsultaController.java
@@ -0,0 +1,44 @@
+package med.voll.api.controller;
+
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import med.voll.api.domain.consulta.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("consultas")
+public class ConsultaController {
+
+    @Autowired
+    ConsultaService consultaService;
+
+    @Autowired
+    ConsultaRepository consultaRepository;
+
+    @Autowired
+    AgendaDeConsultas agendaDeConsultas;
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity agendar(@RequestBody @Valid DadosAgendamentoConsulta dados) {
+        var consulta = consultaService.agendar(dados);
+        return ResponseEntity.ok(new DadosDetalhamentoConsulta(consulta));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity detalharConsulta(@PathVariable Long id){
+        var dadosConsulta = consultaRepository.getReferenceById(id);
+        System.out.println(dadosConsulta);
+        return ResponseEntity.ok(new DadosDetalhamentoConsulta(dadosConsulta));
+    }
+
+    @DeleteMapping
+    @Transactional
+    public ResponseEntity cancelar(@RequestBody @Valid DadosCancelamentoConsulta dados) {
+        agendaDeConsultas.cancelar(dados);
+        return ResponseEntity.noContent().build();
+    }
+
+}
--- api/src/main/java/med/voll/api/domain/medico/MedicoRepository.java
+++ api/src/main/java/med/voll/api/domain/medico/MedicoRepository.java
@@ -1,12 +1,32 @@
 package med.voll.api.domain.medico;
 
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
 
 
 @Repository
 public interface MedicoRepository extends JpaRepository<Medico, Long> {
     Page<Medico> findAllByAtivoTrue(Pageable paginacao);
+
+    @Query("""
+            SELECT m FROM Medico m
+            WHERE 
+            m.ativo = true 
+            AND 
+            m.especialidade = :especialidade
+            AND
+            m.id not in(
+                select c.medico.id from Consulta c where c.data = :data 
+            )
+            ORDER BY RANDOM()
+            LIMIT 1
+            """)
+    Medico escolherMedicoAleatorioNaData(Especialidade especialidade, @NotNull @Future LocalDateTime data);
 }

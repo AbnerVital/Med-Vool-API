2fe7f06864b7e43a46b81507b5ecca0867ce170e
Abner Vital <avgchaves@gmail.com>
2025-07-16 17:22:58 -0300

    feat(validacao): impede agendamentos com menos de 30 minutos de antecedência

--- api/src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorHorarioAntecedencia.java
+++ api/src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorHorarioAntecedencia.java
@@ -1,4 +1,26 @@
 package med.voll.api.domain.consulta.validacoes;
 
-public class ValidadorHorarioAntecedencia {
+import med.voll.api.domain.ValidacaoException;
+import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Component
+public class ValidadorHorarioAntecedencia implements ValidadorAgendamentoDeConsultas{
+
+    public void validar(DadosAgendamentoConsulta dados){
+        var horarioConsulta = dados.data();
+        var agora = LocalDateTime.now();
+        var diferencaEmMinutos = Duration.between(agora, horarioConsulta).toMinutes();
+
+        if(diferencaEmMinutos < 30){
+            throw new ValidacaoException("Consulta deve ser agendada com 30 minutos de antecedência");
+        }
+
+
+
+    }
 }
